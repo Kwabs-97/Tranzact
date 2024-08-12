e9fc2b64903097a1216fdaf3bf5b9b5a646ea48e
Kwabs-github <107767920+Kwabs-97@users.noreply.github.com>
2024-08-12 07:32:47 +0000

    hompeage loading setup start

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function loading() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <p className="text-blue-600">Welcome to Tranz@</p>
+    </div>
+  );
+}
+
+export default loading;
---
+++ src/components/home/Hompage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Hompage() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <header>
+        <h4>Hi there 👋</h4>
+        <p>Welcome to Tranz@</p>
+      </header>
+    </div>
+  );
+}
+
+export default Hompage;
