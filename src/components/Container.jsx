70ca32675894e72f5fe13ff8ad83dc45d451e012
Kwabs-github <107767920+Kwabs-97@users.noreply.github.com>
2024-08-14 04:34:10 +0000

    container from mt top to justify center

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,7 +1,7 @@
 function Container({ children }) {
   return (
     <div
-      className="w-screen h-screen flex items-center mt-20 flex-col
+      className="w-screen h-screen flex items-center justify-center flex-col
     "
     >
       {children}
