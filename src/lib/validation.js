e8dd74d00990afc500d01ad8b02cf544c5563b7a
Kwabs-github <107767920+Kwabs-97@users.noreply.github.com>
2024-08-22 08:41:40 +0000

    zod and register button

--- src/lib/validation.js
+++ src/lib/validation.js
@@ -3,18 +3,12 @@ import * as z from "zod";
 const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
 export const User = z.object({
-  firstname: z
+  username: z
     .string({
-      required_error: "Please your firstname",
+      required_error: "Please your username",
     })
-    .trim()
-    .optional(),
-  lastname: z
-    .string({
-      required_error: "Please your lastname",
-    })
-    .trim()
-    .optional(),
+    .min(4, { message: "Username should be at lease 4 characters" })
+    .trim(),
   password: z
     .string({ required_error: "Password is required" })
     .min(8, { message: "Password should contain at least 8 characters" })
